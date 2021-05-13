b01244c9ba94ec006794314b2cf74f55d492c7dc
Jacob <jacsjs-dev@pm.me>
2021-05-13 15:08:39 +0200

    update .gitignore and fix structure

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,11 @@ fn main() {
     let key = args.next().unwrap(); //<< Unwrapping the string from Option<String>, may cause the program to panic.
             //^^ Option is a model to tell if a thing is there or not.
 
-    let value = args.next().unwrap(); // No such thing as null in normal rust
-    
-    println!("The key is '{}' and the value is '{}'", key, value);
-}
+    let value = args.next().expect("Value was not there"); // Write a panic message with expect(). thread 'main' panicked at 'Value was not there';
 
+    println!("The key is '{}' and the value is '{}'", key, value); //Macro
+
+    let contents = format!("{}\t{}\n", key, value); // << Similar to println! macro, but outputs to string instead.
+    std::fs::write("kv.db", contents);
+
+}
